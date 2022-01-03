712068f25301b2598b972808d4a72cc316c5c498
Isak Styf <isak.styf@masarinconsulting.se>
2022-01-03 16:49:10 +0100

    Added a first version of ExerciseTrail entity type

--- pkg/datamodels/diwise/constants.go
+++ pkg/datamodels/diwise/constants.go
@@ -1,6 +1,14 @@
 package diwise
 
 const (
+	// ExerciseTrailIDPrefix contains the mandatory prefix for Exercise Trail ID
+	ExerciseTrailIDPrefix string = "urn:ngsi-ld:" + ExerciseTrailTypeName + ":"
+	// ExerciseTrailTypeName contains the Exercise Trail type name to reduce hard coding
+	ExerciseTrailTypeName string = "ExerciseTrail"
 	// RoadSurfaceObservedIDPrefix contains the mandatory prefix for Road Surfaces Observed
 	RoadSurfaceObservedIDPrefix string = "urn:ngsi-ld:RoadSurfaceObserved:"
+	// TrailSegmentIDPrefix contains the mandatory prefix for Trail Segment ID
+	TrailSegmentIDPrefix string = "urn:ngsi-ld:" + TrailSegmentTypeName + ":"
+	// TrailSegmentTypeName contains the Trail Segment type name to reduce hard coding
+	TrailSegmentTypeName string = "TrailSegment"
 )
