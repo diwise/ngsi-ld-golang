575a5cff9482659222f0af895d3dc2f2efd33806
shestyf <sofia.styf@masarinconsulting.se>
2022-01-05 12:15:40 +0100

    adding fiware type for water consumption

--- pkg/datamodels/fiware/fiware_test.go
+++ pkg/datamodels/fiware/fiware_test.go
@@ -6,10 +6,40 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	"github.com/diwise/ngsi-ld-golang/pkg/ngsi-ld/types"
 )
 
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
+}
+
+func TestUnmarshalJSONToWaterConsumptionObserved(t *testing.T) {
+	wco := WaterConsumptionObserved{}
+
+	err := json.Unmarshal([]byte(wcoJson), &wco)
+	if err != nil {
+		t.Errorf("Expectation failed. Could not unmarshal json string into WaterConsumptionObserved because: %s", err.Error())
+	}
+
+}
+
+func TestNewWaterConsumptionObserved(t *testing.T) {
+	id := "waterConsumption01"
+
+	wco := NewWaterConsumptionObserved(id)
+
+	wco.WaterConsumption = &WCONumberProperty{
+		NumberProperty: *types.NewNumberPropertyFromInt(806040),
+	}
+
+	if wco.ID != WaterConsumptionObservedIDPrefix+id {
+		t.Error("Expectation failed. Wrong ID.")
+	}
+
+	if wco.WaterConsumption.Value != 806040 {
+		t.Error("Expectation failed. Value of water consumption does not match")
+	}
 }
 
 func TestRoadSegment(t *testing.T) {
@@ -125,4 +155,143 @@ const jsonStrNoLocation string = `{
 		  "type": "Property",
 		  "value": "38"
 	}
+}`
+
+const wcoJson string = `{
+    "id": "urn:ngsi-ld:WaterConsumptionObserved:Consumer01",
+    "type": "WaterConsumptionObserved",
+    "acquisitionStageFailure": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 0,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmFlowPersistence": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": "Nothing to report",
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmInProgress": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 1,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmMetrology": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 1,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmStopsLeaks": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 0,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmSystem": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 1,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmTamper": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 0,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmWaterQuality": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 0,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "location": {
+        "type": "GeoProperty",
+        "value": {
+            "type": "Point",
+            "coordinates": [
+                -4.128871,
+                50.95822
+            ]
+        }
+    },
+    "maxFlow": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 620,
+        "observedAt": "2021-05-23T23:14:16.000Z",
+        "unitCode": "E32"
+    },
+    "minFlow": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 1,
+        "observedAt": "2021-05-23T23:14:16.000Z",
+        "unitCode": "E32"
+    },
+    "moduleTampered": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 1,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "persistenceFlowDuration": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": "3h < 6h",
+        "observedAt": "2021-05-23T23:14:16.000Z",
+        "unitCode": "HUR"
+    },
+    "waterConsumption": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 191051,
+        "observedAt": "2021-05-23T23:14:16.000Z",
+        "unitCode": "LTR"
+    },
+    "@context": [
+        "https://raw.githubusercontent.com/easy-global-market/ngsild-api-data-models/master/WaterSmartMeter/jsonld-contexts/waterSmartMeter-compound.jsonld"
+    ]
 }`
