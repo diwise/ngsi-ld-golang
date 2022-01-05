d5a9f42c7645ef6d4d9f0047c75605e38f888272
Sofia Styf <sofia.styf@masarinconsulting.se>
2022-01-05 14:24:15 +0100

    Feature/waterconsumption (#3)
    * adding fiware type for water consumption
    
    * added waterconsumption constants for id prefix and type name
    
    * created helper function to add consumption to wco

--- pkg/datamodels/fiware/fiware_test.go
+++ pkg/datamodels/fiware/fiware_test.go
@@ -6,10 +6,36 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	"github.com/matryer/is"
 )
 
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
+}
+
+func TestUnmarshalJSONToWaterConsumptionObserved(t *testing.T) {
+	is := is.New(t)
+	wco := WaterConsumptionObserved{}
+
+	err := json.Unmarshal([]byte(wcoJson), &wco)
+
+	is.NoErr(err) // Could not unmarshal json string into WaterConsumptionObserved
+
+}
+
+func TestNewWaterConsumptionObserved(t *testing.T) {
+	is := is.New(t)
+	id := "waterConsumption01"
+	observedAt := time.Now().UTC()
+
+	wco := NewWaterConsumptionObserved(id)
+
+	wco.WithConsumption(id, 806040.0, observedAt)
+
+	is.Equal(wco.ID, WaterConsumptionObservedIDPrefix+id)         // Expected wco.ID to match
+	is.Equal(wco.WaterConsumption.NumberProperty.Value, 806040.0) // Value of water consumption does not match
+
 }
 
 func TestRoadSegment(t *testing.T) {
@@ -125,4 +151,143 @@ const jsonStrNoLocation string = `{
 		  "type": "Property",
 		  "value": "38"
 	}
+}`
+
+const wcoJson string = `{
+    "id": "urn:ngsi-ld:WaterConsumptionObserved:Consumer01",
+    "type": "WaterConsumptionObserved",
+    "acquisitionStageFailure": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 0,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmFlowPersistence": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": "Nothing to report",
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmInProgress": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 1,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmMetrology": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 1,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmStopsLeaks": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 0,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmSystem": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 1,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmTamper": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 0,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "alarmWaterQuality": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 0,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "location": {
+        "type": "GeoProperty",
+        "value": {
+            "type": "Point",
+            "coordinates": [
+                -4.128871,
+                50.95822
+            ]
+        }
+    },
+    "maxFlow": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 620,
+        "observedAt": "2021-05-23T23:14:16.000Z",
+        "unitCode": "E32"
+    },
+    "minFlow": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 1,
+        "observedAt": "2021-05-23T23:14:16.000Z",
+        "unitCode": "E32"
+    },
+    "moduleTampered": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 1,
+        "observedAt": "2021-05-23T23:14:16.000Z"
+    },
+    "persistenceFlowDuration": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": "3h < 6h",
+        "observedAt": "2021-05-23T23:14:16.000Z",
+        "unitCode": "HUR"
+    },
+    "waterConsumption": {
+        "type": "Property",
+        "observedBy": {
+            "type": "Relationship",
+            "object": "urn:ngsi-ld:Device:01"
+        },
+        "value": 191051,
+        "observedAt": "2021-05-23T23:14:16.000Z",
+        "unitCode": "LTR"
+    },
+    "@context": [
+        "https://raw.githubusercontent.com/easy-global-market/ngsild-api-data-models/master/WaterSmartMeter/jsonld-contexts/waterSmartMeter-compound.jsonld"
+    ]
 }`
---
+++ pkg/datamodels/fiware/waterconsumptionobserved.go
@@ -0,0 +1,155 @@
+package fiware
+
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+	"time"
+
+	"github.com/diwise/ngsi-ld-golang/pkg/ngsi-ld/geojson"
+	ngsi "github.com/diwise/ngsi-ld-golang/pkg/ngsi-ld/types"
+)
+
+type WCOTextProperty struct {
+	ngsi.TextProperty
+	ObservedBy ngsi.SingleObjectRelationship `json:"observedBy,omitempty"`
+	ObservedAt string                        `json:"observedAt,omitempty"`
+	UnitCode   string                        `json:"unitCode,omitempty"`
+}
+
+type WCONumberProperty struct {
+	ngsi.NumberProperty
+	ObservedBy ngsi.SingleObjectRelationship `json:"observedBy,omitempty"`
+	ObservedAt string                        `json:"observedAt,omitempty"`
+	UnitCode   string                        `json:"unitCode,omitempty"`
+}
+
+//WaterConsumptionObserved is a fiware entity
+type WaterConsumptionObserved struct {
+	ngsi.BaseEntity
+	AlarmFlowPersistence    *WCOTextProperty         `json:"alarmFlowPersistence,omitempty"`
+	AlarmInProgress         *WCONumberProperty       `json:"alarmInProgress,omitempty"`
+	AlarmMetrology          *WCONumberProperty       `json:"alarmMetrology,omitempty"`
+	AlarmStopsLeaks         *WCONumberProperty       `json:"alarmStopsLeaks,omitempty"`
+	AlarmSystem             *WCONumberProperty       `json:"alarmSystem,omitempty"`
+	AlarmTamper             *WCONumberProperty       `json:"alarmTamper,omitempty"`
+	AlarmWaterQuality       *WCONumberProperty       `json:"alarmWaterQuality,omitempty"`
+	MaxFlow                 *WCONumberProperty       `json:"maxFlow,omitempty"`
+	MinFlow                 *WCONumberProperty       `json:"minFlow,omitempty"`
+	ModuleTampered          *WCONumberProperty       `json:"moduleTampered,omitempty"`
+	PersistenceFlowDuration *WCOTextProperty         `json:"persistenceFlowDuration,omitempty"`
+	WaterConsumption        *WCONumberProperty       `json:"waterConsumption,omitempty"`
+	Location                *geojson.GeoJSONProperty `json:"location,omitempty"`
+}
+
+type waterConsumptionObservedDTO struct {
+	ngsi.BaseEntity
+	AlarmFlowPersistence    *WCOTextProperty   `json:"alarmFlowPersistence,omitempty"`
+	AlarmInProgress         *WCONumberProperty `json:"alarmInProgress,omitempty"`
+	AlarmMetrology          *WCONumberProperty `json:"alarmMetrology,omitempty"`
+	AlarmStopsLeaks         *WCONumberProperty `json:"alarmStopsLeaks,omitempty"`
+	AlarmSystem             *WCONumberProperty `json:"alarmSystem,omitempty"`
+	AlarmTamper             *WCONumberProperty `json:"alarmTamper,omitempty"`
+	AlarmWaterQuality       *WCONumberProperty `json:"alarmWaterQuality,omitempty"`
+	MaxFlow                 *WCONumberProperty `json:"maxFlow,omitempty"`
+	MinFlow                 *WCONumberProperty `json:"minFlow,omitempty"`
+	ModuleTampered          *WCONumberProperty `json:"moduleTampered,omitempty"`
+	PersistenceFlowDuration *WCOTextProperty   `json:"persistenceFlowDuration,omitempty"`
+	WaterConsumption        *WCONumberProperty `json:"waterConsumption,omitempty"`
+	Location                json.RawMessage    `json:"location,omitempty"`
+}
+
+//NewWaterConsumptionObserved creates a new instance of WaterConsumptionObserved
+func NewWaterConsumptionObserved(id string) *WaterConsumptionObserved {
+	if !strings.HasPrefix(id, WaterConsumptionObservedIDPrefix) {
+		id = WaterConsumptionObservedIDPrefix + id
+	}
+
+	wco := &WaterConsumptionObserved{
+		BaseEntity: ngsi.BaseEntity{
+			ID:   id,
+			Type: WaterConsumptionObservedTypeName,
+			Context: []string{
+				"https://raw.githubusercontent.com/easy-global-market/ngsild-api-data-models/master/WaterSmartMeter/jsonld-contexts/waterSmartMeter-compound.jsonld",
+			},
+		},
+	}
+
+	return wco
+}
+
+func (wco *WaterConsumptionObserved) UnmarshalJSON(data []byte) error {
+	dto := &waterConsumptionObservedDTO{}
+	err := json.NewDecoder(bytes.NewReader(data)).Decode(dto)
+
+	if err == nil {
+		wco.ID = dto.ID
+		wco.Type = dto.Type
+		wco.Context = dto.Context
+
+		if dto.AlarmFlowPersistence != nil {
+			wco.AlarmFlowPersistence = dto.AlarmFlowPersistence
+		}
+
+		if dto.AlarmInProgress != nil {
+			wco.AlarmInProgress = dto.AlarmInProgress
+		}
+
+		if dto.AlarmMetrology != nil {
+			wco.AlarmMetrology = dto.AlarmMetrology
+		}
+
+		if dto.AlarmStopsLeaks != nil {
+			wco.AlarmStopsLeaks = dto.AlarmStopsLeaks
+		}
+
+		if dto.AlarmSystem != nil {
+			wco.AlarmSystem = dto.AlarmSystem
+		}
+
+		if dto.AlarmTamper != nil {
+			wco.AlarmTamper = dto.AlarmTamper
+		}
+
+		if dto.AlarmWaterQuality != nil {
+			wco.AlarmWaterQuality = dto.AlarmWaterQuality
+		}
+
+		if dto.MaxFlow != nil {
+			wco.MaxFlow = dto.MaxFlow
+		}
+
+		if dto.MinFlow != nil {
+			wco.MinFlow = dto.MinFlow
+		}
+
+		if dto.ModuleTampered != nil {
+			wco.ModuleTampered = dto.ModuleTampered
+		}
+
+		if dto.PersistenceFlowDuration != nil {
+			wco.PersistenceFlowDuration = dto.PersistenceFlowDuration
+		}
+
+		if dto.WaterConsumption != nil {
+			wco.WaterConsumption = dto.WaterConsumption
+		}
+
+		if dto.Location != nil {
+			wco.Location = geojson.CreateGeoJSONPropertyFromJSON(dto.Location)
+		}
+	}
+
+	return err
+}
+
+func (wco *WaterConsumptionObserved) WithConsumption(device string, consumption float64, observedAt time.Time) *WaterConsumptionObserved {
+	wco.WaterConsumption = &WCONumberProperty{
+		ObservedAt:     observedAt.Format(time.RFC3339),
+		ObservedBy:     *ngsi.NewSingleObjectRelationship(device),
+		NumberProperty: *ngsi.NewNumberProperty(consumption),
+		UnitCode:       "LTR",
+	}
+
+	return wco
+}
