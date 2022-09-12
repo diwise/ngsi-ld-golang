50724e636124559a4e33cacdee6f190cd2eb323c
shestyf <sofia.styf@masarinconsulting.se>
2022-09-12 14:45:15 +0200

    added citywork dto and unmarshal json function

--- pkg/datamodels/fiware/citywork.go
+++ pkg/datamodels/fiware/citywork.go
@@ -1,6 +1,9 @@
 package fiware
 
 import (
+	"bytes"
+	"encoding/json"
+
 	geojson "github.com/diwise/ngsi-ld-golang/pkg/ngsi-ld/geojson"
 	ngsitypes "github.com/diwise/ngsi-ld-golang/pkg/ngsi-ld/types"
 )
@@ -8,6 +11,16 @@ import (
 type CityWork struct {
 	ngsitypes.BaseEntity
 	Location     *geojson.GeoJSONProperty    `json:"location,omitempty"`
+	Description  *ngsitypes.TextProperty     `json:"description,omitempty"`
+	DateCreated  ngsitypes.DateTimeProperty  `json:"dateCreated"`
+	DateModified *ngsitypes.DateTimeProperty `json:"dateModified,omitempty"`
+	EndDate      ngsitypes.DateTimeProperty  `json:"endDate"`
+	StartDate    ngsitypes.DateTimeProperty  `json:"startDate"`
+}
+
+type cityworkDTO struct {
+	ngsitypes.BaseEntity
+	Location     json.RawMessage             `json:"location,omitempty"`
 	Description  *ngsitypes.TextProperty     `json:"description,omitempty"`
 	DateCreated  ngsitypes.DateTimeProperty  `json:"dateCreated"`
 	DateModified *ngsitypes.DateTimeProperty `json:"dateModified,omitempty"`
@@ -28,4 +41,26 @@ func NewCityWork(entityID string) CityWork {
 	}
 
 	return cw
+}
+
+func (c *CityWork) UnmarshalJSON(data []byte) error {
+	dto := &cityworkDTO{}
+	err := json.NewDecoder(bytes.NewReader(data)).Decode(dto)
+
+	if err == nil {
+		c.ID = dto.ID
+		c.Type = dto.Type
+		c.Description = dto.Description
+
+		c.DateCreated = dto.DateCreated
+		c.DateModified = dto.DateModified
+		c.StartDate = dto.StartDate
+		c.EndDate = dto.EndDate
+
+		c.Context = dto.Context
+
+		c.Location = geojson.CreateGeoJSONPropertyFromJSON(dto.Location)
+	}
+
+	return err
 }
